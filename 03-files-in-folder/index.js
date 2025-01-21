64d577c87b0b8693fd3b626340a2a449198bfb5c
Amir <a.abdramanov@aster.kz>
2025-01-21 23:55:09 +0500

    feat: files in folder implementation

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const secretFolderPath = path.join(__dirname, 'secret-folder');
+
+(async () => {
+  try {
+    const files = await fs.readdir(secretFolderPath, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const filePath = path.join(secretFolderPath, file.name);
+        const stats = await fs.stat(filePath);
+
+        const fileName = path.parse(file.name).name;
+        const fileExtension = path.extname(file.name).slice(1);
+        const fileSizeInKB = (stats.size / 1024).toFixed(3);
+
+        console.log(`${fileName} - ${fileExtension} - ${fileSizeInKB}kb`);
+      }
+    }
+  } catch (err) {
+    console.error('Error reading the folder');
+  }
+})();
